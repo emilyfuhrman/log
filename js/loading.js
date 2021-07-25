f2d7f9e43902167f70bb87e28cbaf74f6915b8c7
emilyfuhrman <emily.c.fuhrman@gmail.com>
2021-07-24 18:56:36 -0700

    try

--- js/loading.js
+++
@@ -1,7 +0,0 @@
-//fade in once all images are done
-var $content = $('.container#main');
-
-$content.imagesLoaded().always(function(){
-	$('#loading-animation').addClass('hide');
-	$content.addClass('show');
-});
