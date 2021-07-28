c384d3376cd882654000ab1c21a2bfc882f4fb52
emilyfuhrman <emily.c.fuhrman@gmail.com>
2021-07-27 20:00:50 -0800

    load background images

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,7 @@
 //fade in once all images are done
 var $content = $('.container#log');
 
-$content.imagesLoaded().always(function(){
+$content.imagesLoaded({ background: true }).always(function(){
 	$('#loading-animation').addClass('hide');
 	$content.addClass('show');
 });
