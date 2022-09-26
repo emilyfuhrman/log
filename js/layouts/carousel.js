b1214dae490c034b17e7e6db6e7010961ad2553a
emilyfuhrman <emily.c.fuhrman@gmail.com>
2022-09-26 16:16:13 -0400

    even more progress

--- js/layouts/carousel.js
+++ js/layouts/carousel.js
@@ -2,45 +2,46 @@
  * Simple jQuery Infinite Carousel
  * SOURCE: https://codepen.io/matthewwilliams/pen/Vayrjv
  */
-var focus_id = '_0';
-var carousel = $('#' +focus_id +' .carousel'),
-    threshold = 150,
+var threshold = 150,
     slideWidth = 500,
     dragStart, 
     dragEnd;
 
-$('#' +focus_id +' .next').click(function(){ shiftSlide(-1) })
-$('#' +focus_id +' .prev').click(function(){ shiftSlide(1) })
+$('.next').click(function(){ shiftSlide(this,-1) })
+$('.prev').click(function(){ shiftSlide(this, 1) })
 
-carousel.on('mousedown', function(){
-  if (carousel.hasClass('transition')) return;
-  dragStart = event.pageX;
-  $(this).on('mousemove', function(){
-    dragEnd = event.pageX;
-    $(this).css('transform','translateX('+ dragPos() +'px)')
-  })
-  $(document).on('mouseup', function(){
-    if (dragPos() > threshold) { return shiftSlide(1) }
-    if (dragPos() < -threshold) { return shiftSlide(-1) }
-    shiftSlide(0);
-  })
-});
+// carousel.on('mousedown', function(){
+//   if (carousel.hasClass('transition')) return;
+//   dragStart = event.pageX;
+//   $(this).on('mousemove', function(){
+//     dragEnd = event.pageX;
+//     $(this).css('transform','translateX('+ dragPos() +'px)')
+//   })
+//   $(document).on('mouseup', function(){
+//     if (dragPos() > threshold) { return shiftSlide(1) }
+//     if (dragPos() < -threshold) { return shiftSlide(-1) }
+//     shiftSlide(0);
+//   })
+// });
 
 function dragPos() {
   return dragEnd - dragStart;
 }
 
-function shiftSlide(direction) {
+function shiftSlide(_elem, _direction) {
+  var focus_id = $(_elem).parent().attr('id');
+  var carousel = $('#' +focus_id +' .carousel');
+
   if (carousel.hasClass('transition')) return;
   dragEnd = dragStart;
   $(document).off('mouseup')
   carousel.off('mousemove')
           .addClass('transition')
-          .css('transform','translateX(' + (direction * slideWidth) + 'px)'); 
+          .css('transform','translateX(' + (_direction * slideWidth) + 'px)'); 
   setTimeout(function(){
-    if (direction === 1) {
+    if (_direction === 1) {
       $('#' +focus_id +' .slide:first').before($('#' +focus_id +' .slide:last'));
-    } else if (direction === -1) {
+    } else if (_direction === -1) {
       $('#' +focus_id +' .slide:last').after($('#' +focus_id +' .slide:first'));
     }
     carousel.removeClass('transition')
